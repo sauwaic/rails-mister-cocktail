7b4ed1249f4e730c07da8f72b3aec3679102c88c
Sau Wai Chan <sauwaic@gmail.com>
2016-11-18 15:22:13 +0100

    Nav bar edited

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -3,6 +3,7 @@ class Cocktail < ApplicationRecord
   has_many :doses, dependent: :destroy
 
   validates :name, presence: true, uniqueness: true
+  validates :photo, presence: true
 
   mount_uploader :photo, PhotoUploader
 end
